90ca27191a4af5f3a14ceb2d6ce2322ae5aa6a2d
BRANNAN.SAPIENS <jamiebrannan2@gmail.com>
2026-04-02 14:52:27 +0200

    feat: start empty playground

---
+++ performance-debate/ConditionalHidden.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: A UIKit based Playground for presenting user interface
+
+import SwiftUI
+import PlaygroundSupport
+
+struct MyViewController: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.setLiveView(MyViewController())
+
